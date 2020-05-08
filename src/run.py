988c5f89e9f5ac7ce4e8d9237722404fd54075fb
Supriti Sinhamahapatra <goodpiku@gmail.com>
2020-05-08 13:41:41 +0200

    updated version

--- src/run.py
+++ src/run.py
@@ -3,8 +3,10 @@ from Song import Song
 import evaluation as eva
 
 content = pd.read_csv('../benchmark/fewsongs.csv', delimiter=',')
-content = content.sample(frac=1).reset_index(drop=True)# shuffle data
+content = content.sample(frac=1).reset_index(drop=True)  # shuffle data
 dict_artistnames_to_indx = {}
+
+
 def get_artist_to_idx(artist: str) -> int:
     """
     dictionary where the keys are artist_names and values are unique numbers.
@@ -16,7 +18,9 @@ def get_artist_to_idx(artist: str) -> int:
         dict_artistnames_to_indx[artist] = len(dict_artistnames_to_indx)
     return dict_artistnames_to_indx[artist]
 
-content['artist_id'] = content['artist'].apply(get_artist_to_idx) # a new column gets created in the csv file with unique artist_id.
+
+content['artist_id'] = content['artist'].apply(
+    get_artist_to_idx)  # a new column gets created in the csv file with unique artist_id.
 list_of_song_instances = []
 for i in range(len(content)):
     """
@@ -25,11 +29,8 @@ for i in range(len(content)):
     """
     s = Song(label=content.iloc[i][0], artist_id=content.iloc[i][4], song_name=content.iloc[i][1],
              lyrics=content.iloc[i][3])
-    #print (s)
+    # print (s)
     list_of_song_instances.append(s)
-
-
-
 
 list_of_artist_frequency = list(content['artist_id'])
 list_of_labels = list(content['artist_id'])
@@ -45,4 +46,3 @@ macro_scores_dict = eva.macro_scores(evaluation)
 
 print(micro_scores_dict)
 print(macro_scores_dict)
-
