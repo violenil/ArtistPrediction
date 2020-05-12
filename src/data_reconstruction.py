8b833458b134855f2b4a129a36433dd63cdfbb3e
violenil <retrospect@protonmail.com>
2020-05-12 15:54:29 +0200

    Filter_content function complete

--- src/data_reconstruction.py
+++ src/data_reconstruction.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from typing import List, Dict, Tuple
 
 def filter_content(data:pd.DataFrame)->pd.DataFrame:
     """
@@ -6,5 +7,19 @@ def filter_content(data:pd.DataFrame)->pd.DataFrame:
     @param data:
     @return:
     """
+    dict_of_artist_song_freq = {}
+    all_artists = data['artist']
+    for n, artist in all_artists.items():
+        if artist in dict_of_artist_song_freq.keys():
+            dict_of_artist_song_freq[artist] += 1
+        else:
+            dict_of_artist_song_freq[artist] = 1
+    
+    reduced_dict = {artist:freq for artist, freq in dict_of_artist_song_freq.items() if freq > 150}
 
-    return data
+    print(reduced_dict)
+    return reduced_dict
+
+if __name__ == '__main__':
+    content = pd.read_csv('../benchmark/songdata.csv', delimiter=',')
+    filter_content(content)
