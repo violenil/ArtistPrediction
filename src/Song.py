9af7949fc0286c13d58e7f6f9436377f544a87b8
violenil <retrospect@protonmail.com>
2020-05-28 21:41:27 +0200

    changed tokenization and pos tagging, using nltk

--- src/Song.py
+++ src/Song.py
@@ -1,5 +1,5 @@
 from typing import List, Tuple, Dict, Set
-
+from nltk import word_tokenize, pos_tag
 import re
 
 
@@ -7,9 +7,11 @@ class Song:
     def __init__(self, label: str, artist_id: int, song_name: str, lyrics: str) -> None:
         self.label = label
         self.artist_id = artist_id
-        self.song_name = self.get_tokenized_data(song_text=song_name)
-        shortend_lyrics = self.get_tokenized_data(song_text=lyrics)
+        self.song_name = self.word_tokenize(song_text=song_name)
+        shortend_lyrics = self.word_tokenize(song_text=lyrics)
         self.lyrics = shortend_lyrics
+        tagged_lyrics = pos_tag(shortend_lyrics) #returns list of tuples [(word, tag), ...]
+        self.tagged_lyrics = dict((y, x) for x, y in tagged_lyrics) # makes into dict {tag:word, tag:word, ...}
         self.feature_vector = []
 
     def __str__(self) -> str:
@@ -23,7 +25,6 @@ class Song:
         tokens = [match.group(0) for match in
                   re.finditer(r"\w+|([^\w])\1*", song_text)]  # keeps punctuation and \n chars
 
-        # tokens = word_tokenize(song_text)
 
         # tokens = song_text.split(' ')
         # tokens = get_tokens(text=song_text, chars=[' ', ',', "'"])
@@ -43,6 +44,8 @@ class Song:
         # assert len(vocab) == len(feat_vec)
         self.feature_vector = feat_vec
     
+    def extract_emotions(self, 
+
     def extract_unique_song_features(self, nouns: Dict) -> None:
         """
         This method extracts a set of features:
@@ -55,8 +58,7 @@ class Song:
             counts for 5 punctuation symbols (',','.','!','?',''')
         """
         feat_vec = []
-        # TO DO
-            
+        # TO DO    
 
 if __name__ == '__main__':
     s = Song('The Beatles', 1, 'Hills of green',
